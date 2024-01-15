4699297391159f66c42db8a5085c1fbfdd907041
Philipp Kant <philipp.kant@minafoundation.com>
2024-01-15 17:21:12 +0100

    Add some typedoc documentation

--- packages/token/src/interfaces/admin/admin.ts
+++ packages/token/src/interfaces/admin/admin.ts
@@ -4,6 +4,13 @@ import type { Bool } from 'o1js';
 import type { AdminAction } from '../token/adminable';
 
 interface Admin {
+  /**
+   * Check whether you have permission to perform a given {@link AdminAction}.
+   *
+   * @param action
+   * @returns A {@link Bool} which is `true` iff you have the right to
+   *   perform `action`.
+   */
   canAdmin: (action: AdminAction) => Bool;
 }
 
