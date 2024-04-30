bc2ea51342a7d484b7068e69471afd71b648fe36
Philipp Kant <philipp.kant@minafoundation.com>
2024-04-30 17:41:39 +0200

    Bump o1js version to 1.1.0
    And adapt to the breaking changes

--- test_util.ts
+++ test_util.ts
@@ -1,11 +1,12 @@
-import type { PrivateKey, PublicKey } from "o1js"
+import { PrivateKey, PublicKey } from "o1js"
+import { TestPublicKey } from "o1js/dist/node/lib/mina/local-blockchain"
 
 export type TestAccount = {
   publicKey: PublicKey
   privateKey: PrivateKey
 }
 
-export type TestAccounts = ArrayOfLength<TestAccount, 10>
+export type TestPublicKeys = ArrayOfLength<TestPublicKey, 10>
 
 type ArrayOfLength<T, L extends number, A extends T[] = []> = number extends L ? T[]
   : L extends A["length"] ? A
