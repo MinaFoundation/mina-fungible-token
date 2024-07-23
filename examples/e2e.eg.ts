b47402d8b3e1a599ac2645e4cb82ffa4d6ffbd2a
Philipp Kant <philipp.kant@minafoundation.com>
2024-07-23 11:36:00 +0200

    Move initialising permissions to `deploy()`

--- examples/e2e.eg.ts
+++ examples/e2e.eg.ts
@@ -24,12 +24,10 @@ const deployTx = await Mina.transaction({
 }, async () => {
   AccountUpdate.fundNewAccount(deployer, 3)
   await adminContract.deploy({ adminPublicKey: admin.publicKey })
-  adminContract.init()
   await token.deploy({
     symbol: "abc",
     src: "https://github.com/MinaFoundation/mina-fungible-token/blob/main/FungibleToken.ts",
   })
-  token.init()
   await token.initialize(
     admin.publicKey,
     UInt8.from(9),
